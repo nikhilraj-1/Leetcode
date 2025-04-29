c17efddf67ee4617e2bc5b844f2c3afaa885a877
Nikhil Raj <111196019+nikhilraj-1@users.noreply.github.com>
2025-04-30 01:12:46 +0530

    Time: 3 ms (90.95%), Space: 21.3 MB (61.82%) - LeetHub

--- 0347-top-k-frequent-elements/0347-top-k-frequent-elements.py
+++ 0347-top-k-frequent-elements/0347-top-k-frequent-elements.py
@@ -3,12 +3,13 @@ class Solution:
         hm={}
         for num in nums:
             hm[num]=1+hm.get(num,0)
-        arr=[]
-        for num,count in hm.items():
-            arr.append([count,num])
-        arr.sort(key = lambda x:x[0])
-
+        heap=[]
+        for n in hm.keys():
+            heapq.heappush(heap,[hm[n],n])
+            if len(heap)>k:
+                heapq.heappop(heap)
         res=[]
-        while len(res)<k:
-            res.append(arr.pop()[1])
+        for i in range(k):
+            res.append(heapq.heappop(heap)[1])
         return res
+    
