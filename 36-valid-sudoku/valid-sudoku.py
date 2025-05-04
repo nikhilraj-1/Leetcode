ba3f2db0f4475ac89b275ada5ddc391e7dc46dfc
Nikhil Raj <111196019+nikhilraj-1@users.noreply.github.com>
2025-05-04 14:18:25 +0530

    Time: 6 ms (45.47%) | Memory: 17.7 MB (92.24%) - LeetSync

--- 36-valid-sudoku/valid-sudoku.py
+++ 36-valid-sudoku/valid-sudoku.py
@@ -1,33 +1,18 @@
 class Solution:
     def isValidSudoku(self, board: List[List[str]]) -> bool:
-        for row in range(9):
-            seen = set()
-            for i in range(9):
-                if board[row][i] == ".": 
+        rows = defaultdict(set)
+        cols = defaultdict(set)
+        squares = defaultdict(set)
+
+        for i in range(9):
+            for j in range(9):
+                if board[i][j]==".":
                     continue
-                if board[row][i] in seen:
+                if board[i][j] in rows[i] or  board[i][j] in cols[j] or board[i][j] in squares[(i//3,j//3)]:
                     return False
-                seen.add(board[row][i])
-        
-        for col in range(9):
-            seen = set()
-            for i in range(9):
-                if board[i][col] == ".":
-                    continue
-                if board[i][col] in seen:
-                    return False
-                seen.add(board[i][col])
-            
-        for square in range(9):
-            seen = set()
-            for i in range(3):
-                for j in range(3):
-                    row = (square//3) * 3 + i
-                    col = (square % 3) * 3 + j
-                    if board[row][col] == ".":
-                        continue
-                    if board[row][col] in seen:
-                        return False
-                    seen.add(board[row][col])
+                else:
+                    rows[i].add(board[i][j])
+                    cols[j].add(board[i][j])
+                    squares[(i//3,j//3)].add(board[i][j])
         return True
         
