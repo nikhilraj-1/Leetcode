384a3d8d1ba673a3e029cf270ef301bb134145a6
Nikhil Raj <111196019+nikhilraj-1@users.noreply.github.com>
2025-06-01 11:04:05 +0530

    Time: 48 ms (85.94%) | Memory: 22.1 MB (44.46%) - LeetSync

--- 236-lowest-common-ancestor-of-a-binary-tree/lowest-common-ancestor-of-a-binary-tree.py
+++ 236-lowest-common-ancestor-of-a-binary-tree/lowest-common-ancestor-of-a-binary-tree.py
@@ -7,21 +7,14 @@
 
 class Solution:
     def lowestCommonAncestor(self, root: 'TreeNode', p: 'TreeNode', q: 'TreeNode') -> 'TreeNode':
-        path1,path2=[],[]
-        self.findpath(root,p,path1)
-        self.findpath(root,q,path2)
-        ans = root
-        for i in range (min(len(path1),len(path2))):
-            if path1[i].val==path2[i].val:
-                ans = path1[i]
-        return ans
-    def findpath(self, root:'TreeNode',n:'TreeNode',path:['TreeNode']):
         if not root:
-            return False
-        path.append(root)
-        if root.val==n.val:
-            return True
-        if self.findpath(root.left,n,path) or self.findpath(root.right,n,path) :
-            return True
-               
-        path.pop()
+            return None
+        if root.val == p.val or root.val ==q.val:
+            return root
+        leftV = self.lowestCommonAncestor(root.left,p,q)
+        rightV = self.lowestCommonAncestor(root.right,p,q)
+        if leftV and rightV:
+            return root
+        if not leftV :
+            return rightV
+        return leftV
