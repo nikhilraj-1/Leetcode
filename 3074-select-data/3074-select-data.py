549d2b2932ef9b2b20986c54fc0548a3744fe2c5
Nikhil Raj <111196019+nikhilraj-1@users.noreply.github.com>
2025-08-19 17:57:56 +0530

    Time: 366 ms (5.05%), Space: 67 MB (67.58%) - LeetHub

--- 3074-select-data/3074-select-data.py
+++ 3074-select-data/3074-select-data.py
@@ -1,7 +1,9 @@
 import pandas as pd
 
 def selectData(students: pd.DataFrame) -> pd.DataFrame:
-    temp = students[students['student_id'] == 101][['name', 'age']]
+    # df_idx= students.set_index('student_id')
+    # temp = df_idx.loc[101,['name', 'age']]
+    temp =students.loc[students['student_id']==101,['name', 'age']]
     return temp
 
 
