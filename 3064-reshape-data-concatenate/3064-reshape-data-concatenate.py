f6d0277ea43ccf82218e0d88e81af18af7a9a30b
Nikhil Raj <111196019+nikhilraj-1@users.noreply.github.com>
2025-08-19 18:59:35 +0530

    Time: 378 ms (5.1%), Space: 66.5 MB (74.56%) - LeetHub

--- 3064-reshape-data-concatenate/3064-reshape-data-concatenate.py
+++ 3064-reshape-data-concatenate/3064-reshape-data-concatenate.py
@@ -1,6 +1,6 @@
 import pandas as pd
 
 def concatenateTables(df1: pd.DataFrame, df2: pd.DataFrame) -> pd.DataFrame:
-    res = pd.concat([df1,df2],axis=0,ignore_index=True)
+    res = pd.concat([df1,df2],axis=0)
     return res
     
