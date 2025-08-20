50252014d8768d6a29072836e0213ada2e505cc5
Nikhil Raj <111196019+nikhilraj-1@users.noreply.github.com>
2025-08-20 15:41:04 +0530

    Time: 63 ms (46.2%), Space: 20.4 MB (52.6%) - LeetHub

--- 1402-count-square-submatrices-with-all-ones/1402-count-square-submatrices-with-all-ones.py
+++ 1402-count-square-submatrices-with-all-ones/1402-count-square-submatrices-with-all-ones.py
@@ -1,19 +1,20 @@
 class Solution:
     def countSquares(self, matrix: List[List[int]]) -> int:
-        row = len(matrix)
-        col = len(matrix[0])
-        res=0
-        cache = {}
-        def dfs(r,c):
-            if r>=row or c>=col or matrix[r][c]==0:
-                return 0
-            if (r,c) in cache :
-                return cache[(r,c)]
-            cache[(r,c)]=1+min(dfs(r+1,c),dfs(r,c+1),dfs(r+1,c+1))
-            return cache[(r,c)]
-        
+        row,col = len(matrix),len(matrix[0])
+        dp =[]
+        for _ in range (row):
+            dp.append([0]*col)
+        for r in range (row):
+            dp[r][0]=matrix[r][0]
+        for c in range (col):
+            dp[0][c]=matrix[0][c]
+        for i in range (1,row):
+            for j in range(1,col):
+                if matrix[i][j]==1:
+                    dp[i][j] = 1 + min(dp[i-1][j],dp[i][j-1],dp[i-1][j-1])
+        ans =0
         for i in range (row):
             for j in range(col):
-                res+=dfs(i,j)
-        return res
-        
+                ans += dp[i][j]
+
+        return ans
