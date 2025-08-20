0201670c97b480d95ce667c634a47084ca68e00e
Nikhil Raj <111196019+nikhilraj-1@users.noreply.github.com>
2025-08-20 15:14:00 +0530

    Time: 207 ms (11.36%), Space: 38.3 MB (8.28%) - LeetHub

--- 1402-count-square-submatrices-with-all-ones/1402-count-square-submatrices-with-all-ones.py
+++ 1402-count-square-submatrices-with-all-ones/1402-count-square-submatrices-with-all-ones.py
@@ -1,23 +1,19 @@
 class Solution:
     def countSquares(self, matrix: List[List[int]]) -> int:
-        m= len(matrix)
-        n = len(matrix[0])
-        count =0
-
-        def isValid(r,c,size):
-            for i in range (r,r+size):
-                for j in range(c,c+size):
-                    if i >= m or j>=n or matrix[i][j]==0:
-                        return False
-            return True
-
-        for i in range (m):
-            for j in range (n) :
-                if matrix[i][j]==1:
-                    count+=1
-                    size=2
-                    while(isValid(i,j,size)):
-                        count+=1
-                        size+=1
-        return count
+        row = len(matrix)
+        col = len(matrix[0])
+        res=0
+        cache = {}
+        def dfs(r,c):
+            if r>=row or c>=col or matrix[r][c]==0:
+                return 0
+            if (r,c) in cache :
+                return cache[(r,c)]
+            cache[(r,c)]=1+min(dfs(r+1,c),dfs(r,c+1),dfs(r+1,c+1))
+            return cache[(r,c)]
+        
+        for i in range (row):
+            for j in range(col):
+                res+=dfs(i,j)
+        return res
         
