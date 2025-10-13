47eaf0b5809e51bfba24355e6a93d8c4716fd60d
Nikhil Raj <111196019+nikhilraj-1@users.noreply.github.com>
2025-10-13 14:22:13 +0530

    Time: 6 ms (53.93%), Space: 17.8 MB (47.41%) - LeetHub

--- 1353-find-resultant-array-after-removing-anagrams/1353-find-resultant-array-after-removing-anagrams.py
+++ 1353-find-resultant-array-after-removing-anagrams/1353-find-resultant-array-after-removing-anagrams.py
@@ -1,12 +1,13 @@
 class Solution:
-    def removeAnagrams(self, words: List[str]) -> List[str]:
-        def isAnagram(s1,s2):
+    def isAnagram(self,s1,s2):
             if len(s1)!=len(s2):
                 return False
             return sorted(s1)==sorted(s2)
+
+    def removeAnagrams(self, words: List[str]) -> List[str]:       
         i=1
         while i< len(words):
-            if isAnagram(words[i-1],words[i]):
+            if self.isAnagram(words[i-1],words[i]):
                 words.pop(i)
             else:
                 i+=1
